ce7306a844f0309ceaa1dff1199059e1bb7db84f
Anirudh Saxena <anirudhsaxenaa1310@gmail.com>
2026-09-22 09:10:08 +0530

    jerney to devsecops

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -50,8 +50,8 @@ echo "📁 Setting up project..."
 sudo mkdir -p /var/www/jerney
 sudo chown -R $USER:$USER /var/www/jerney
 
-# Copy project files (assumes you've transferred them to ~/Jerney)
-cp -r ~/Jerney/* /var/www/jerney/
+# Copy project files (assumes you've transferred them to ~/DevSecOps)
+cp -r ~/DevSecOps/* /var/www/jerney/
 
 # --- Install backend dependencies ---
 echo "📦 Installing backend dependencies..."
